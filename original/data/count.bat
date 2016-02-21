7f2c292f4ecdc496b16cb6f65e8b6f83c9b60bcd
Jonghoon Seo <jonghoon.seo@gmail.com>
2016-02-21 19:06:06 +0900

    Modify speed data to show std. dev.

---
+++ original/data/count.bat
@@ -0,0 +1,3 @@
+echo %1 >> count.txt
+echo %2 >> count.txt
+"C:\Programming\Temporary Works\Color Pixel Counter\debug\Color Pixel Counter.exe" "C:\Papers\Working\Writing On\Enhanced ISBF\실험 데이터\두번째 실험\데스크탑 결과\%1\%2\tracer02.bmp" >> count.txt
---
+++ original/data/run.bat
@@ -0,0 +1,1 @@
+for %%a in (SBF,MSBF,ISBF,EISBF,MNT,RSA) do for %%f in (f01_200,f02_200,f03_200,f04_200,f05_200,f06_200,f07_200,f08_200,f10_200) do count %%a %%f
